7311f09053070683db8d46f0d013f4a08415f9e9
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-11-06 21:59:42 +0000

    Split off PyOP2 benchmark abstract base class

---
+++ adv_diff_bench.py
@@ -0,0 +1,88 @@
+import os
+import pylab
+
+from benchrun import clock
+from pyop2_bench import PyOP2Benchmark
+from generate_mesh import generate_meshfile
+import parameters
+
+class AdvDiffBenchmark(PyOP2Benchmark):
+    """PyOP2 vs. Fluidity vs. DOLFIN benchmark."""
+
+    # Execute for all combinations of these parameters
+    parameters = ['version', 'meshsize']
+    version = ['fluidity', 'fluidity_pyop2', 'pyop2', 'dolfin']
+    meshsize = [10 * 2**i for i in range(6)]
+
+    # Compare timings against this parameter value
+    reference = ('version', 'fluidity')
+
+    def dolfin(self, meshsize):
+        os.system("python dolfin_adv_diff.py %d" % meshsize)
+
+    def fluidity(self, meshsize):
+        os.system('${FLUIDITY_DIR}/bin/fluidity advection_diffusion.%d.flml' % meshsize)
+
+    def fluidity_pyop2(self, meshsize):
+        os.system('${FLUIDITY_DIR}/bin/fluidity ufl_advection_diffusion.%d.flml' % meshsize)
+
+    def pyop2(self, meshsize):
+        os.system('python pyop2_adv_diff.py -m mesh_%d' % meshsize)
+
+    def create_input(self):
+        for s in self.meshsize:
+            mesh = 'mesh_%d' % s
+            # Generate triangle mesh
+            generate_meshfile(mesh, s)
+            # Generate flml
+            for flml in ['advection_diffusion', 'ufl_advection_diffusion']:
+                with open('%s.flml.template'%flml) as f1, open('%s.%d.flml'%(flml,s), 'w') as f2:
+                    f2.write(f1.read() % {
+                        'mesh': mesh,
+                        'diffusivity': parameters.diffusivity,
+                        'current_time': parameters.current_time,
+                        'dt': parameters.dt,
+                        'endtime': (parameters.endtime - parameters.dt)
+                        })
+
+    def run(self, version, meshsize):
+        print "Running %s with mesh size %dx%d" % (version, meshsize, meshsize)
+        t1 = clock()
+        self.__getattribute__(version)(meshsize)
+        t =  clock() - t1
+        elems = 2*meshsize*meshsize
+        if not elems in self.plotdata['elements']:
+            self.plotdata['elements'].append(elems)
+        self.plotdata[version].append(t)
+        return t
+
+    def plot_result(self):
+        title = 'Benchmark of an advection-diffusion problem for 100 time steps'
+        for fig, pl in zip(('linear', 'semilogx'), (pylab.plot, pylab.semilogx)):
+            self._plot('runtime_'+fig, pl, lambda x: x, 'upper left', 'Overall runtime in seconds', title)
+            self._plot('speedup_'+fig, pl, lambda x: x+'_speedup', 'lower right', 'Relative speedup over Fluidity baseline', title)
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser(description=AdvDiffBenchmark.__doc__)
+    parser.add_argument('-r', '--run', action='store_true', help='run benchmarks')
+    parser.add_argument('-p', '--plot', action='store_true', help='plot results')
+    parser.add_argument('-d', '--dump', action='store_true', help='Pickle dump')
+    parser.add_argument('-c', '--csv', action='store_true', help='Dump to CSV')
+    parser.add_argument('-l', '--load', help='Pickle load from file')
+    args = parser.parse_args()
+
+    b = AdvDiffBenchmark()
+    if args.load and os.path.exists(args.load):
+        b.load(args.load)
+    if args.run:
+        b.create_input()
+        b.print_result()
+        b.compute_speedup()
+    if args.dump:
+        b.dump()
+    if args.csv:
+        b.write_csv()
+    if args.plot:
+        b.plot_result()
--- pyop2_bench.py
+++ pyop2_bench.py
@@ -1,5 +1,3 @@
-# PyOP2 vs. Fluidity vs. DOLFIN benchmark
-
 import os
 import csv
 import pickle
@@ -7,64 +5,15 @@ from collections import defaultdict
 from datetime import datetime
 import pylab
 
-from benchrun import Benchmark, clock
-from generate_mesh import generate_meshfile
-import parameters
-
-def dolfin(meshsize):
-    os.system("python dolfin_adv_diff.py %d" % meshsize)
-
-def fluidity(meshsize):
-    os.system('${FLUIDITY_DIR}/bin/fluidity advection_diffusion.%d.flml' % meshsize)
-
-def fluidity_pyop2(meshsize):
-    os.system('${FLUIDITY_DIR}/bin/fluidity ufl_advection_diffusion.%d.flml' % meshsize)
-
-def pyop2(meshsize):
-    os.system('python pyop2_adv_diff.py -m mesh_%d' % meshsize)
+from benchrun import Benchmark
 
 class PyOP2Benchmark(Benchmark):
-    """PyOP2 vs. Fluidity vs. DOLFIN benchmark."""
-
-    # Execute for all combinations of these parameters
-    parameters = ['version', 'meshsize']
-    version = ['fluidity', 'fluidity_pyop2', 'pyop2', 'dolfin']
-    meshsize = [10 * 2**i for i in range(6)]
-
-    # Compare timings against this parameter value
-    reference = ('version', 'fluidity')
+    """Abstract base class for PyOP2 benchmarks."""
 
     def __init__(self):
-        Benchmark.__init__(self)
+        super(PyOP2Benchmark, self).__init__()
         self.plotdata = defaultdict(list)
         self.timestamp = datetime.today().isoformat().replace(':', '')
-
-    def create_input(self):
-        for s in self.meshsize:
-            mesh = 'mesh_%d' % s
-            # Generate triangle mesh
-            generate_meshfile(mesh, s)
-            # Generate flml
-            for flml in ['advection_diffusion', 'ufl_advection_diffusion']:
-                with open('%s.flml.template'%flml) as f1, open('%s.%d.flml'%(flml,s), 'w') as f2:
-                    f2.write(f1.read() % {
-                        'mesh': mesh,
-                        'diffusivity': parameters.diffusivity,
-                        'current_time': parameters.current_time,
-                        'dt': parameters.dt,
-                        'endtime': parameters.endtime - parameters.dt
-                        })
-
-    def run(self, version, meshsize):
-        print "Running %s with mesh size %dx%d" % (version, meshsize, meshsize)
-        t1 = clock()
-        globals()[version](meshsize)
-        t =  clock() - t1
-        elems = 2*meshsize*meshsize
-        if not elems in self.plotdata['elements']:
-            self.plotdata['elements'].append(elems)
-        self.plotdata[version].append(t)
-        return t
 
     def _path(self, filename):
         if not os.path.exists(self.timestamp):
@@ -86,7 +35,7 @@ class PyOP2Benchmark(Benchmark):
 
     def compute_speedup(self):
         for v in self.version:
-            for this, base in zip(self.plotdata[v], self.plotdata['fluidity']):
+            for this, base in zip(self.plotdata[v], self.plotdata[self.reference[1]]):
                 self.plotdata[v+'_speedup'].append(base/this)
 
     def _plot(self, fig, plot, col, legend_pos, ylabel, title, show=False):
@@ -102,34 +51,3 @@ class PyOP2Benchmark(Benchmark):
         if show:
             pylab.show()
         pylab.close(f)
-
-    def plot_result(self):
-        title = 'Benchmark of an advection-diffusion problem for 100 time steps'
-        for fig, pl in zip(('linear', 'semilogx'), (pylab.plot, pylab.semilogx)):
-            self._plot('runtime_'+fig, pl, lambda x: x, 'upper left', 'Overall runtime in seconds', title)
-            self._plot('speedup_'+fig, pl, lambda x: x+'_speedup', 'lower right', 'Relative speedup over Fluidity baseline', title)
-
-if __name__ == '__main__':
-
-    import argparse
-    parser = argparse.ArgumentParser(description=PyOP2Benchmark.__doc__)
-    parser.add_argument('-r', '--run', action='store_true', help='run benchmarks')
-    parser.add_argument('-p', '--plot', action='store_true', help='plot results')
-    parser.add_argument('-d', '--dump', action='store_true', help='Pickle dump')
-    parser.add_argument('-c', '--csv', action='store_true', help='Dump to CSV')
-    parser.add_argument('-l', '--load', help='Pickle load from file')
-    args = parser.parse_args()
-
-    b = PyOP2Benchmark()
-    if args.load and os.path.exists(args.load):
-        b.load(args.load)
-    if args.run:
-        b.create_input()
-        b.print_result()
-        b.compute_speedup()
-    if args.dump:
-        b.dump()
-    if args.csv:
-        b.write_csv()
-    if args.plot:
-        b.plot_result()

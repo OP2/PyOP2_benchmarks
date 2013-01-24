ec244053dfcb0f85cc7607c2a77b9a7a3ec47b43
gmarkall <grm08@doc.ic.ac.uk>
2013-01-24 13:58:19 +0000

    Use a unifrom distribution of mesh sizes

--- adv_diff_bench.py
+++ adv_diff_bench.py
@@ -37,7 +37,8 @@ class AdvDiffBenchmark(PyOP2Benchmark):
 
     def __init__(self, np=1, message='', version=None, reference=None, meshsize=None, parameters=None):
         # Execute for all combinations of these parameters
-        self.meshsize = meshsize or [int(10 * sqrt(2)**i) for i in range(11)]
+        self.meshsize = meshsize or [101, 142, 174, 201, 225, 246, 266, 284, 301, 317, 333, 347, 362, 375, 388]
+        #[int(10 * sqrt(2)**i) for i in range(11)]
         parameters = parameters or ['version', 'meshsize']
         self.version = version or ['fluidity', 'fluidity_pyop2_seq', 'pyop2_seq']
         self.primed = []

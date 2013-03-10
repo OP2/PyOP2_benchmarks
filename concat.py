6c839db1e7af38b2b299a075b89c7992909db793
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-03-10 21:06:43 +0000

    Modularize concat.py

--- concat.py
+++ concat.py
@@ -4,10 +4,14 @@ import sys
 from glob import glob
 from pstats import Stats
 
-if len(sys.argv) != 3:
-    print __doc__
-    sys.exit(1)
-files = glob(sys.argv[1])
-s = Stats(files[0])
-for f in files[1:]: s.add(f)
-s.dump_stats(sys.argv[2])
+def concat(pattern, outfile):
+    files = glob(pattern)
+    s = Stats(files[0])
+    for f in files[1:]: s.add(f)
+    s.dump_stats(outfile)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print __doc__
+        sys.exit(1)
+    concat(sys.argv[1], sys.argv[2])

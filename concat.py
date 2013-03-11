91d64c30652dcda23464542e12083dc882c4ac61
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-03-11 09:44:14 +0000

    Make concat MPI aware

--- concat.py
+++ concat.py
@@ -1,19 +1,28 @@
-"""Usage: run PATTERN FILE"""
+"""Concatenate cProfile results from multiple files.
+
+Usage: run PATTERN FILE [mpi]
+"""
 
 import os
 import sys
 from glob import glob
 from pstats import Stats
 
-def concat(pattern, outfile):
+def concat(pattern, outfile, mpi=None):
+    if mpi:
+        from mpi4py import MPI
+        pattern = pattern % MPI.COMM_WORLD.rank
+        outfile = outfile % MPI.COMM_WORLD.rank
     files = glob(pattern)
-    s = Stats(files[0])
-    for f in files[1:]: s.add(f)
-    s.dump_stats(outfile)
-    os.remove(files)
+    if files:
+        s = Stats(files[0])
+        for f in files[1:]: s.add(f)
+        s.dump_stats(outfile)
+        for f in files:
+            os.remove(f)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print __doc__
         sys.exit(1)
-    concat(sys.argv[1], sys.argv[2])
+    concat(sys.argv[1], sys.argv[2], len(sys.argv) == 4)
